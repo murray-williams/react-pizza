5a3350e178df9569fdcd21e8333f9203c1c7ea5f
Murray Williams <murray@waterfalldigital.co.za>
2021-12-07 11:12:34 +0200

    Setup styled components with global style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: blue;
+  }
+`
 
 function App() {
-  return <div className="App">React Pizza</div>
+  return (
+    <>
+      <GlobalStyle>
+        <div className="App">React Pizza</div>
+      </GlobalStyle>
+    </>
+  )
 }
 
 export default App
