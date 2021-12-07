0ceeb27a2c230c639dad62167286bfe4b1b6e041
Murray Williams <murray@waterfalldigital.co.za>
2021-12-07 11:27:52 +0200

    Creted logo styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components'
 import { pizzaRed } from '../Styles/colors'
+import { Title } from '../Styles/title'
 
 const NavbarStyled = styled.div`
   background-color: ${pizzaRed};
+  padding: 10px;
+`
+
+const Logo = styled(Title)`
+  font-size: 20px;
+  color: white;
 `
 
 export default function Navbar() {
-  return <NavbarStyled>Hello Navbar</NavbarStyled>
+  return (
+    <NavbarStyled>
+      <Logo>Sliceline</Logo>
+    </NavbarStyled>
+  )
 }
