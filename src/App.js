9e2c3b76893dc73f1711d2a007fa036ae4a758ff
Murray Williams <murray@waterfalldigital.co.za>
2021-12-07 11:20:54 +0200

    Setup colors & navbar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import React from 'react'
 import { createGlobalStyle } from 'styled-components'
+
+// components
+import Navbar from './components/Navbar'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: blue;
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
   }
 `
 
 function App() {
   return (
     <>
-      <GlobalStyle>
-        <div className="App">React Pizza</div>
-      </GlobalStyle>
+      <Navbar />
+      <h1>Sliceline</h1>
+      <GlobalStyle />
+      <div className="App">React Pizza</div>
     </>
   )
 }
